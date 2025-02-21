0f61ca8732cd085538b3e590c16c0f8de4098e82
YSB1026 <103161220+YSB1026@users.noreply.github.com>
2025-02-21 09:55:40 +0900

    2025/02/20 출력

---
+++ 2025_0220_Study1/2025_0220_Study1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2025_0220_Study1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("멋사4기 출력 성공");
+        }
+    }
+}
