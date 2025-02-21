09db7a01739107ca3148eccf1da678b9bcd9efdd
YSB1026 <103161220+YSB1026@users.noreply.github.com>
2025-02-21 09:55:09 +0900

    github desktop 사용하기
    2025/02/21 github desktop 사용하기

---
+++ 2025_0221_study2/2025_0221_likeLion1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2025_0221_likeLion1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("깃허브 연결");
+        }
+    }
+}
