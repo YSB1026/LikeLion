78aca216aad6703cc0533ad58ac77dd92c5f1f76
YSB1026 <103161220+YSB1026@users.noreply.github.com>
2025-02-21 10:23:47 +0900

    정수형 변수

---
+++ 2025_0221_likeLion2/2025_0221_likeLion2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2025_0221_likeLion2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //변수선언
+            int age = 26;
+
+            Console.WriteLine(age);
+        }
+    }
+}
