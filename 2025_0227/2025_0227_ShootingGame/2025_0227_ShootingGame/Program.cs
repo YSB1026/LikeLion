d05c7bbd1cb29ea8366e4f4a6abbae3ada40846d
YSB1026 <103161220+YSB1026@users.noreply.github.com>
2025-02-27 21:35:18 +0900

    슈팅게임
    불필요한 using문 제거

--- 2025_0227/2025_0227_ShootingGame/2025_0227_ShootingGame/Program.cs
+++ 2025_0227/2025_0227_ShootingGame/2025_0227_ShootingGame/Program.cs
@@ -1,14 +1,9 @@
 ﻿using System;
-using System.CodeDom;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Runtime.InteropServices.ComTypes;
-using System.Security.Permissions;
 using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
-using System.Xml.Linq;
+
 
 namespace _2025_0227_ShootingGame
 {
